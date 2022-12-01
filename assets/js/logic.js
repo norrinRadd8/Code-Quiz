85722acf0f068465d988ade79d9a8277e35277a6
H <h@Hs-MacBook-Air.local>
2022-12-01 12:57:12 +0000

    added timeCountdown function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,23 @@
-timerEl = document.getElementById('time')
+var timerEl = document.getElementById("time");
+var startEl = document.querySelector(".start");
 
-console.log(timerEl)
+function timeCountDown() {
+    var timeLeft = 120;
+
+    var timeInerval = setInterval(function() {
+        timerEl.innerText = timeLeft;
+        timeLeft--;
+        if(timeLeft < 0) {
+            clearTimeout(timeInerval);
+        }
+    }, 1200)
+    
+}
+
+startEl.addEventListener("click", function(event) {
+    timeCountDown()
+    event.preventDefault()
+    console.log(event)
+})
+
+
