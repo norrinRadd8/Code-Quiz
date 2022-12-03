1a73e1c7719ee7ec8157d02d7ac52cdeeab61b58
H <h@Hs-MacBook-Air.local>
2022-12-03 22:00:18 +0000

    code update

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,10 +6,9 @@ var questionTitle = document.querySelector("#question-title");
 var choicesOutput = document.querySelector("#choices");
 var endScreen = document.querySelector("#end-screen")
 var questionsHide = document.querySelector("#questions")
-//var answer = " "
-var currentQuestionIndex = 0
+var currentQuestionIndex = Math.floor(Math.random() * 3)
 var currentQuestion = triviaData[currentQuestionIndex]
-var check = currentQuestion.answer
+
 
 
 //Function for the timer
@@ -27,35 +26,32 @@ function timeCountDown() {
     
 }
 
-startEl.addEventListener("click", function(event) {
-    event.preventDefault()
+function startQuiz(event) {
     timeCountDown()
+    event.preventDefault()
     startEl.innerText = ''
+    questionLoop()
+}
 
+ function questionLoop() {
     
     var choices = currentQuestion.choices
 
     questionTitle.innerText = currentQuestion.question
-    
     choicesOutput.innerHTML = ''
     questionsHide.classList.remove('hide')
 
     for(var i = 0; i < choices.length; i++) {
      var choice = choices[i]
      var isCorrect = currentQuestion.answer === choice
-     //var check = choice == answer
-     
-        //console.log(check)
-    //  console.log(typeof choice)
-    //  console.log(typeof currentQuestion.answer)
     
      choicesOutput.insertAdjacentHTML('beforeend', `
     <button data-correct=${isCorrect}>${choice}</button>
      `)
-     
+     currentQuestionIndex
     }    
-})
-
+    //console.log(event)
+}
 
 function checkCorrectAnswer(event) {
     
@@ -64,41 +60,29 @@ function checkCorrectAnswer(event) {
     var feedBack = document.querySelector("#feedback")
     
     feedBack.classList.remove('hide')
-    
-    //console.log(element)
+   
 
     if(dataCorrect === "true") {
         feedBack.innerText = 'Correct!'
-    } else feedBack.innerText = 'Incorrect!'
-
-    // for(var i = 0; i < triviaData.length; i++) {
-    //     var check = triviaData[i].answer
-    //     //console.log(check)
+            
+    } 
+    if(dataCorrect != "true") {
+        feedBack.innerText = 'Incorrect!'
         
-    // }
-    // if(check === "3. alerts") {
-    //     feedBack.innerText = `Correct!`
-    //     //console.log(dataCorrect)
-        
-    //     console.log(check)  
-        
-    //  } 
-   
-    //console.log(dataCorrect)
-    //console.log(check)
-    //console.log('Correct')
-    //console.log(event)
-    //console.log(dataCorrect)
-    //console.log(check)
+    }  
+    questionLoop(event)
     
-    
-
-    //triviaData[0].answer
-    
-    
+    console.log(typeof currentQuestionIndex) 
+    console.log(typeof feedBack)
    
 }
+
+// function displayFeedback() {
+
+// }
+
 choicesOutput.addEventListener('click', checkCorrectAnswer) 
+startEl.addEventListener('click', startQuiz)
 
 
 
