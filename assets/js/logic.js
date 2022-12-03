5209792ad4732a3c1ab28a9da51bea25c0a1d758
H <h@Hs-MacBook-Air.local>
2022-12-03 17:32:49 +0000

    check correct answer code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,8 +5,11 @@ var wrapper = document.querySelector(".wrapper");
 var questionTitle = document.querySelector("#question-title");
 var choicesOutput = document.querySelector("#choices");
 var endScreen = document.querySelector("#end-screen")
-var questionsHide = document.querySelector('#questions')
+var questionsHide = document.querySelector("#questions")
+var answer = " "
 var currentQuestionIndex = 0
+var currentQuestion = triviaData[currentQuestionIndex]
+var check = currentQuestion.answer
 
 
 //Function for the timer
@@ -28,39 +31,84 @@ startEl.addEventListener("click", function(event) {
     event.preventDefault()
     timeCountDown()
     startEl.innerText = ''
-    var currentQuestion = triviaData[currentQuestionIndex]
+
     
     var choices = currentQuestion.choices
 
-    questionsHide.classList.remove('hide')
     questionTitle.innerText = currentQuestion.question
     
     choicesOutput.innerHTML = ''
+    questionsHide.classList.remove('hide')
 
     for(var i = 0; i < choices.length; i++) {
      var choice = choices[i]
      var isCorrect = currentQuestion.answer === choice
+     
+     
+    //  console.log(typeof choice)
+    //  console.log(typeof currentQuestion.answer)
     
      choicesOutput.insertAdjacentHTML('beforeend', `
     <button data-correct=${isCorrect}>${choice}</button>
      `)
      
-    }
-    
-    
+    }    
 })
 
+
 function checkCorrectAnswer(event) {
-    console.log(event.target)
-    var dataCorrect = element.dataset.correct = ""
+    
+    var element = event.target
+    var dataCorrect = element.getAttribute("data-correct")
+    var feedBack = document.querySelector("#feedback")
+    
+
+    feedBack.classList.remove('hide')
+    
+    console.log(element)
+    // for(var i = 0; i < choices.length; i++) {
+    //     var check = [i].answer
+    //     console.log(check)
+    // }
+    if(dataCorrect = 'true') {
+        feedBack.innerText = `Correct!`
+        //console.log(dataCorrect)
+        
+        
+        
+    } 
+    console.log(typeof dataCorrect)
+    //console.log(check)
+    //console.log('Correct')
+    //console.log(event)
+    //console.log(dataCorrect)
+    //console.log(check)
+    
+    
+
+    //triviaData[0].answer
+    
+    
+   
 }
-choicesOutput.addEventListener('click', checkCorrectAnswer)
+choicesOutput.addEventListener('click', checkCorrectAnswer) 
+
+
+
+// function currentQuestion() {
+    
+// }
+
+// currentQuestion()
+
+
 // if(){
 
 // }
-checkCorrectAnswer()
-if(element.dataset.correct === true) {
-    console.log('CORRECT')
-}
-
+// checkCorrectAnswer()
+// if(element.dataset.correct === true) {
+//     console.log('CORRECT')
+// }
+//console.log(triviaData[0].answer)
+// 
 
