1ab79fbc4c0bfd28d133ebeb901488e1a83240df
H <h@Hs-MacBook-Air.local>
2022-12-03 11:38:08 +0000

    added first question code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,15 @@
+// Global variables
 var timerEl = document.getElementById("time");
 var startEl = document.querySelector(".start");
+var wrapper = document.querySelector('.wrapper');
+var questionTitle = document.querySelector("#question-title");
+var choicesOutput = document.querySelector("#choices");
+var currentQuestionIndex = 0
 
+
+//Function for the timer
 function timeCountDown() {
     var timeLeft = 120;
-
     var timeInerval = setInterval(function() {
         timerEl.innerText = timeLeft;
         timeLeft--;
@@ -15,9 +21,47 @@ function timeCountDown() {
 }
 
 startEl.addEventListener("click", function(event) {
-    timeCountDown()
     event.preventDefault()
-    console.log(event)
+    timeCountDown()
+    startEl.innerText = ''
+    var currentQuestion = triviaData[currentQuestionIndex]
+    var questionsHide = document.querySelector('#questions')
+    var choices = currentQuestion.choices
+
+    questionsHide.classList.remove('hide')
+    questionTitle.innerText = currentQuestion.question
+    
+    choicesOutput.innerHTML = ''
+
+    for(var i = 0; i < choices.length; i++) {
+     var choice = choices[i]
+     var isCorrect = currentQuestion.answer === choice
+
+     choicesOutput.insertAdjacentHTML('beforeend', `
+    <button data-correct=${isCorrect}>${choice}</button>
+     `)
+     console.log(choice)
+    }
+    
+    
 })
 
 
+ console.log(questionTitle)
+
+// var title = questionsEl.children[0]
+// questionsEl = 'Hi There!!'
+// var output = questionsEl += title.innerText
+
+// console.log(output)
+
+// function loopQuestions() {
+//     var arr = triviaData
+//     for(var i = 0; i < triviaData; i++) {   
+        
+//     }
+//     console.log(arr[0].question)
+//     console.log(arr[0].choices[2])
+// }
+
+// loopQuestions()
---
+++ assets/js/questions.js
@@ -0,0 +1,24 @@
+// My set of stored questions
+var triviaData = [
+    {
+    question : 'Commonly used data types DO NOT include: ',
+    choices: ['1. strings', '2. booleans', '3. alerts', '4. numbers'], 
+    answer : '3. alerts'
+    },
+    {
+    question : 'The condition in an if / else statement is enclosed within ___',
+    choices : ['1. quotes', '2. curly brackets', '3. parentheses', '4. square brackets'],
+    answer : '3. parentheses'
+    },
+    {
+    question : 'Arrays in JavaScript can be used to store ___',
+    choices : ['1. numbers and strings', '2. other arrays', '3. booleans', '4. all of the above'],
+    answer : '4. all of the above'
+    }
+    
+]
+
+
+
+
+
